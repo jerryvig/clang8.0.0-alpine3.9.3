cc5ed69059b69c19b3c2f523768d59f87972b053
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-11 13:49:53 -0500

    p

--- src/build_Clang.sh
+++ src/build_Clang.sh
@@ -3,15 +3,19 @@
 echo 'Running build_Clang.sh...'
 
 # Download and build llvm
-/usr/bin/curl http://releases.llvm.org/8.0.0/llvm-8.0.0.src.tar.xz -o llvm-8.0.0.src.tar.xz
-/usr/bin/unxz llvm-8.0.0.src.tar.xz
-tar xvf llvm-8.0.0.src.tar
-rm llvm-8.0.0.src.tar
-cd llvm-8.0.0.src
-mkdir build
-cd build
-/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++  ../
-/usr/bin/ninja -C .
+#/usr/bin/curl http://releases.llvm.org/8.0.0/llvm-8.0.0.src.tar.xz -o llvm-8.0.0.src.tar.xz
+#/usr/bin/unxz llvm-8.0.0.src.tar.xz
+#tar xvf llvm-8.0.0.src.tar
+#rm llvm-8.0.0.src.tar
+#cd llvm-8.0.0.src
+#mkdir build
+#cd build
+#/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++  ../
+#/usr/bin/ninja -C .
+
+cd /llvm-8.0.0.src/build
+/usr/bin/ninja install
+cd /
 
 # Download and build clang
 /usr/bin/curl http://releases.llvm.org/8.0.0/cfe-8.0.0.src.tar.xz -o cfe-8.0.0.src.tar.xz
